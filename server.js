177a6b683cdfc6b86136619196135e3abebc615c
lorian-andreisandu <loriansandu@gmail.com>
2024-01-14 22:43:33 +0200

    updated angular

---
+++ server.js
@@ -0,0 +1,18 @@
+function requireHTTPS(req, res, next) {
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
+const express = require('express');
+const app = express();
+
+app.use(requireHTTPS);
+app.use(express.static('./dist/file-share-frontend'));
+
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/file-share-frontend/'}),
+);
+
+app.listen(process.env.PORT || 4200);
