845e018c6cc77fc60b7c323f0dda263bb21567d2
lorian-andreisandu <loriansandu@gmail.com>
2024-01-15 15:04:59 +0200

    changed api url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'http://localhost:8080',
+    // apiUrl: 'http://localhost:8080',
     production: true,
-    // apiUrl: 'https://fileshare-backend-5hf7.onrender.com'
+    apiUrl: 'https://fileshare-backend-5hf7.onrender.com'
 };
